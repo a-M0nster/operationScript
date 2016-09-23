1af6bba294637c363720147076941f7a2fb2d951
monster <m0nster.lanxing@gmail.com>
2016-09-23 15:05:29 +0800

    Update restart_tcpdump.sh

--- restart_tcpdump.sh
+++ restart_tcpdump.sh
@@ -5,3 +5,4 @@ kill -9  $psid
 chmod 777 /root/scan.$(date +%Y%m%d -d "-1 day").pcap
 mv /root/scan.$(date +%Y%m%d -d "-1 day").pcap /var/pcap/
 /usr/sbin/tcpdump -i eth0 -w /root/scan.$(date +%Y%m%d).pcap tcp port 80 or udp &
+
