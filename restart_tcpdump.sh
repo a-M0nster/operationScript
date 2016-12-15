04abb2e56d90aed7e80a33e7dc930e06f91ce9b0
monster <m0nster.lanxing@gmail.com>
2016-12-15 16:23:45 +0800

    filter
    new filter

--- restart_tcpdump.sh
+++ restart_tcpdump.sh
@@ -7,5 +7,5 @@ psid=`ps aux | grep 'tcpdump ' | awk '{print $2}'`
 kill -9  $psid
 chmod 777 /root/scan.$(date +%Y%m%d -d "-1 day").pcap
 mv /root/scan.$(date +%Y%m%d -d "-1 day").pcap /var/pcap/
-/usr/sbin/tcpdump -i eth0 -w /root/scan.$(date +%Y%m%d).pcap tcp port 80 or udp &
+/usr/sbin/tcpdump -i eth0 -w /root/scan.$(date +%Y%m%d).pcap not arp and not ether dst FF:FF:FF:FF:FF:FF and not ip multicast &
 
