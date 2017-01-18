f543e8249cf4dc4692dd68b4e64e87fdc2dbebf8
monster <m0nster.lanxing@gmail.com>
2017-01-18 18:33:38 +0800

    Update restart_tcpdump.sh

--- restart_tcpdump.sh
+++ restart_tcpdump.sh
@@ -7,5 +7,5 @@ psid=`ps aux | grep 'tcpdump ' | awk '{print $2}'`
 kill -9  $psid
 chmod 777 /root/scan.$(date +%Y%m%d -d "-1 day").pcap
 mv /root/scan.$(date +%Y%m%d -d "-1 day").pcap /var/pcap/
-/usr/sbin/tcpdump -i eth0 -w /root/scan.$(date +%Y%m%d).pcap not arp and not ether dst FF:FF:FF:FF:FF:FF and not ip multicast and net xxx &
+/usr/sbin/tcpdump -i eth0 -w /root/scan.$(date +%Y%m%d).pcap not arp and not ether dst FF:FF:FF:FF:FF:FF and not ip multicast and not host xxx &
 
